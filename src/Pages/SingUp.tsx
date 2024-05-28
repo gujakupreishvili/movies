49f7819dd6f19e9bbaed8f84c6d194ecd84c586e
gujakupreishvili <gujakupreishvili4@gmail.com>
2024-05-28 19:43:23 +0400

    finish project

--- src/Pages/SingUp.tsx
+++ src/Pages/SingUp.tsx
@@ -1,4 +1,4 @@
-import logo from "../.././public/assets/logo.png";
+import logo from "../../public/assets/logo.png";
 import { useNavigate, Link } from "react-router-dom";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase/firebaseConfig";
@@ -7,115 +7,123 @@ import { signUpValidationSchema } from "../utils/validationschema";
 
 export default function SignUp() {
   const navigation = useNavigate();
-  const initialValues = {
-    email: "",
-    password: "",
-    password2: "",
-  };
-
-  const handleLink = () => {
-   return navigation("/login", {});
-  };
 
   const formik = useFormik({
-    initialValues: initialValues,
+    initialValues: {
+      email: "",
+      password: "",
+      password2: "",
+    },
+    validationSchema: signUpValidationSchema,
     onSubmit: async (values) => {
       try {
-        await register(auth, values.email, values.password);
+        await register(values.email, values.password);
+        handleLink();
       } catch (error) {
         console.error("Registration error:", error);
       }
     },
-    validationSchema: signUpValidationSchema,
   });
 
-  const register = async (auth: any, email: string, password: string) => {
+  const register = async (email: string, password: string) => {
     try {
       const userCredential = await createUserWithEmailAndPassword(
         auth,
         email,
         password
       );
-      console.log(userCredential);
-      const user = userCredential.user;
-      console.log("User registered:", user);
-      console.log("hello");
+      console.log("User registered:", userCredential.user);
     } catch (error) {
       console.error("Registration error:", error);
+      throw error;
     }
   };
 
-  const { handleChange, handleSubmit, values, errors, handleBlur } = formik;
+  const handleLink = () => {
+    navigation("/login");
+  };
+
+  const checkPassword = () => {
+    if (formik.values.password2 !== formik.values.password) {
+      alert("Passwords do not match.");
+      return false;
+    }
+    return true;
+  };
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    if (checkPassword()) {
+      formik.handleSubmit();
+    }
+  };
+
+  const { handleChange, values, errors, handleBlur } = formik;
 
   return (
-    <>
-      <div className="flex flex-col items-center h-[100vh] justify-center gap-[58px]">
-        <img src={logo} alt="" />
-        <div className="w-[327px] bg-[#161D2F] rounded-[10px] flex flex-col pl-[24px] pr-[24px]">
-          <h1 className="pt-[24px] pb-[40px] text-[32px] text-white font-normal">
-            Sign Up
-          </h1>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="email"
-              name="email"
-              placeholder="Email address"
-              value={values.email}
-              onBlur={handleBlur}
-              onChange={handleChange}
-              className="bg-transparent w-[279px] h-[37px] border-b border-[#5A698F] mb-[24px] outline-none text-white pl-[15px] pb-[17px]"
-            />
-            {errors.email && (
-              <p className="text-red-500 self-start mt-[-20px]">
-                {errors.email}
-              </p>
-            )}
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              value={values.password}
-              onBlur={handleBlur}
-              onChange={handleChange}
-              className="bg-transparent w-[279px] h-[37px] border-b border-[#5A698F] mb-[24px] outline-none text-white pl-[15px] pb-[17px]"
-            />
-            {errors.password && (
-              <p className="text-red-500 self-start mt-[-20px]">
-                {errors.password}
-              </p>
-            )}
-            <input
-              type="password"
-              name="password2"
-              placeholder="Repeat Password"
-              value={values.password2}
-              onBlur={handleBlur}
-              onChange={handleChange}
-              className="bg-transparent w-[279px] h-[37px] border-b border-[#5A698F] mb-[24px] outline-none text-white pl-[15px] pb-[17px]"
-            />
-            {errors.password2 && (
-              <p className="text-red-500 self-start mt-[-20px]">
-                {errors.password2}
-              </p>
-            )}
-            <button
-              type="submit"
-              className="mt-[16px] bg-[#FC4747] h-[48px] text-white rounded-[6px] mb-[26px] text-center  w-[280px]"
-              onClick={() => {
-                handleLink(); handleSubmit()
-              }}
-            >
-              Create an account
-            </button>
-          </form>
-          <p className="text-white font-normal pb-[26px] text-[15px] w-[100%] text-center">
-            Already have an account?
-            <Link to="/login">
-              <span className="text-[#FC4747] pl-[8px]">Login</span>
-            </Link>
-          </p>
-        </div>
+    <div className="flex flex-col items-center h-screen justify-center gap-8">
+      <img src={logo} alt="Logo" />
+      <div className="w-[327px] bg-[#161D2F] rounded-[10px] flex flex-col pl-[24px] pr-[24px]">
+        <h1 className="pt-[24px] pb-[40px] text-[32px] text-white font-normal">
+          Sign Up
+        </h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email address"
+            value={values.email}
+            onBlur={handleBlur}
+            onChange={handleChange}
+            className={`bg-transparent w-[279px] h-[37px] border-b  mb-[24px] outline-none text-white pl-[15px] pb-[17px]${
+              errors.email ? "border-red-500" : "border-[#5A698F]"
+            }`}
+          />
+          {errors.email && (
+            <p className="text-red-500 mt-[-20px]">{errors.email}</p>
+          )}
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={values.password}
+            onBlur={handleBlur}
+            onChange={handleChange}
+            className={`bg-transparent w-[279px] h-[37px] border-b  mb-[24px] outline-none text-white pl-[15px] pb-[17px]${
+              errors.password ? "border-red-500" : "border-[#5A698F]"
+            }`}
+          />
+          {errors.password && (
+            <p className="text-red-500 mt-[-20px]">{errors.password}</p>
+          )}
+          <input
+            type="password"
+            name="password2"
+            placeholder="Repeat Password"
+            value={values.password2}
+            onBlur={handleBlur}
+            onChange={handleChange}
+            className={`bg-transparent w-[279px] h-[37px] border-b  mb-[24px] outline-none text-white pl-[15px] pb-[17px]${
+              errors.password2 ? "border-red-500" : "border-[#5A698F]"
+            }`}
+          />
+          {errors.password2 && (
+            <p className="text-red-500 mt-[-20px]">{errors.password2}</p>
+          )}
+          <button
+            type="submit"
+            className="mt-[16px] w-[279px] bg-[#FC4747] h-[48px] text-white rounded-[6px] mb-[26px]"
+          >
+            Create an account
+          </button>
+        </form>
+        <p className="text-white pb-[24px] pl-[17px]">
+          Already have an account?{" "}
+          <Link to="/login" className="text-red-500">
+            Log in
+          </Link>
+        </p>
       </div>
-    </>
+    </div>
   );
 }
--- src/Pages/favourite/fav.tsx
+++ src/Pages/favourite/fav.tsx
@@ -15,13 +15,13 @@ export function Fav() {
   const cart: Movie[] = location.state ? location.state.cart : []; // Access the cart array from location state
   return (
     <>
-      <div className="flex flex-col lg:flex-row gap-20">
-        <Header cart={cart} />
-        <div className="flex gap-10 mt-[20px]">
+      <div className="flex flex-col lg:flex-row  gap-20">
+        <Header cart={cart} /> 
+        <div className="flex gap-10 mt-[20px] flex-wrap">
           {cart.map((item) => (
             <div
               key={item.id}
-              className="w-[165px] transition-transform duration-[1000ms] relative hover:scale-110 hover:z-20 hover:cursor-pointer hover:shadow-2xl"
+              className="w-[165px] transition-transform duration-[1000ms]  relative hover:scale-110 hover:z-20 hover:cursor-pointer hover:shadow-2xl"
             >
               <img
                 src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
